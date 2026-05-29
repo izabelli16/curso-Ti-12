e14921734b1a2448364d07e699f64199ea7eebd2
izabelli16 <christinyiza@gmail.com>
2026-05-29 14:14:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const body = document.querySelector('body')
+ const escreva = (texto)=> {
+    body.innerHTML = texto + '<br/>'
+}
+
+
+let total
+let contaluz = 300
+let contaAgua = 95
+let contaGás = 200
+let comprasMes = 600 
+
+total = contaluz + contaAgua + contaGás + comprasMes
+
+
+
+
+escreva(nome)
+escreva("Gastos Domésticos - valor total:")
